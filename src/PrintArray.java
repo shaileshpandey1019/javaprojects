5db03959e014b1a22785322ca0b2f78835da3ccf
Shailesh Pandey <shailesh@Shaileshs-MacBook-Air.local>
2024-06-08 11:27:47 +0530

    print array elements

---
+++ src/PrintArray.java
@@ -0,0 +1,9 @@
+public class PrintArray {
+    public static void main(String[] args){
+        int[] ar1 ={1,2,3,5,7,1};
+
+        for(int i = 0; i< ar1.length; i++) {
+            System.out.println(ar1[i]);
+        }
+    }
+}
